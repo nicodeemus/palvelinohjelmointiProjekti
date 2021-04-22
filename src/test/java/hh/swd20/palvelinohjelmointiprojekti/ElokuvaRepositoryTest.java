11eb6501009d1a26002b436c592f2635999536c2
nicodeemus <bgj359@myy.haaga-helia.fi>
2021-04-22 10:01:07 +0300

    final

---
+++ src/test/java/hh/swd20/palvelinohjelmointiprojekti/ElokuvaRepositoryTest.java
@@ -0,0 +1,39 @@
+package hh.swd20.palvelinohjelmointiprojekti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import hh.swd20.palvelinohjelmointiprojekti.domain.Elokuva;
+import hh.swd20.palvelinohjelmointiprojekti.domain.ElokuvaRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ElokuvaRepositoryTest {
+	
+	@Autowired
+	private ElokuvaRepository repository;
+	
+	//elokuvan luonnin testaus
+	@Test
+	public void createNewElokuva() {
+		Elokuva elokuva = new Elokuva();
+		repository.save(elokuva);
+		assertThat(elokuva.getId()).isNotNull();
+	}
+	
+	//elokuvan poiston testaus
+	@Test
+	public void deleteElokuva() {
+		Elokuva elokuva = new Elokuva();
+		repository.deleteAll();
+		
+		assertThat(elokuva.getId()).isNull();
+	}
+	
+}
---
+++ src/test/java/hh/swd20/palvelinohjelmointiprojekti/TagRepositoryTest.java
@@ -0,0 +1,55 @@
+package hh.swd20.palvelinohjelmointiprojekti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import hh.swd20.palvelinohjelmointiprojekti.domain.Tag;
+import hh.swd20.palvelinohjelmointiprojekti.domain.ElokuvaRepository;
+import hh.swd20.palvelinohjelmointiprojekti.domain.Tag;
+import hh.swd20.palvelinohjelmointiprojekti.domain.TagRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TagRepositoryTest {
+	
+	@Autowired
+	private TagRepository repository;
+	
+	
+	//Uuden tagin testaus
+	@Test
+	public void createNewTag() {
+		Tag tag = new Tag();
+		repository.save(tag);
+		assertThat(tag.getTagId()).isNotNull();
+	}
+	
+	//Tagin editoinnin testaus
+	@Test
+	public void editTag() {
+		Tag tag = new Tag("tag1");
+		
+		repository.save(tag);
+		repository.findById(tag.getTagId());
+		tag.setNimi("tag2");
+		repository.save(tag);
+		
+		assertThat(repository.findById(tag.getTagId()).get().getNimi()).isEqualTo("tag2");
+	}
+	
+	//Tagin poiston testaus
+	@Test
+	public void deleteTag() {
+		Tag Tag = new Tag();
+		repository.deleteAll();
+		
+		assertThat(Tag.getTagId()).isNull();
+	}
+	
+}
